74f8cfa7393dc588111f7e4d458b6ece7ade3a11
Unique-chiemerie <lilanimate1@gmail.com>
2024-01-11 02:41:34 +0100

    created the map data

--- lib/data/song_stringpath.dart
+++ lib/data/song_stringpath.dart
@@ -8,5 +8,58 @@ Map<String, String> songPathMap = {
   //Joy to the world
   'Joy to the World': 'Joy to the world_afrobeat.mp3',
   //Baa baa black sheep
-  'Baa,Baa black sheep': 'Baa_black sheep.mp3'
+  'Baa,Baa black sheep': 'Baa_black sheep.mp3',
+  //Mary had a little lamb
+  'Mary had a little lamb': 'Mary had a little lamb.mp3',
+  //old mcdonald
+  'Old mcDonald had a farm': 'old-macdonald-had-a-farm.mp3',
+  //if you're happy and you know it
+  'if you\'re happy and you know it': "if you're happy and you know it.mp3",
+  //Row your boat
+  'Row Row Row your boat': 'Row your boat.mp3',
+  //head shoulders
+  'Head, Shoulders, Knees, and Toes': 'Head-Shoulders-Knees-and-Toes.mp3',
+  //itsy bitsy
+  'Itsy bitsy spider': 'Itsy-Bitsy-Spider-Singing-Bell.mp3',
+  //Alphabet song
+  'Alphabet song': 'alphabet song.mp3',
+  //rain rain
+  'Rain Rain GO away': 'Rain Rain go away.mp3',
+  //Humpty dumpty
+  'Humpty dumpty': 'Humpty-Dumpty.mp3',
+  //The wheels on the bus
+  'The wheels on the bus': 'wheels on the bus.mp3',
+  //London bridge
+  'London bridge is falling down': 'London bridge.mp3',
+  //for he's a jolly good fellow
+  'For he\'s a jolly good fellow': 'For he jolly.mp3',
+  //Jack and Jill
+  'Jack and Jill': 'Jack and jill.mp3',
+  //This old man
+  'This old man': 'this_old_man.mp3',
+  //She'll be coming round the mountain
+  'She\'ll be coming\'round the mountain':
+      "she'll be coming round the mountain.mp3",
+  //pop goes the weasel
+  'Pop goes the weasel': 'Pop-Goes-the-weasel-Singing-Bell.mp3',
+  //The more we get together
+  'The more we get together': 'The-more-we-get-together-Singing-Bell.mp3',
+  //five little monkeys
+  'Five little monkeys': 'Five-little-monkeys-Singing-Bell.mp3',
+  //Ten in the bed
+  'Ten in the bed': 'Ten-in-the-Bed-Singing-Bell.mp3',
+  //i'm a little teapot
+  'I\'m a little teapot': 'I-m-a-little-teapot-Singing-Bell.mp3',
+  //Baby bumblebee
+  'Baby bumblebee': 'Baby-Bumblebee-Singing-Bell.mp3',
+  //Jingle bells
+  'Jingle bells': 'Jingle-Bells-Singing-Bell.mp3',
+  //Deck the halls
+  'Deck the halls': 'Deck-the-halls-Singing-Bell.mp3',
+  //Rock a bye
+  'Rock-a-bye-baby': 'Rock-a-bye-baby.mp3',
+  //Bingo song
+  'Bingo song': 'Bingo-Song-Singing-Bell.mp3',
+  //Hush little baby
+  'Hush little baby': 'Hush-Little-Baby-Singing-Bell.mp3'
 };
